fd97bf65cb3a73e7dceb272ff55ec5f8bec53c2c
Bardia Eghbali <bardia.eghbali@gmail.com>
2021-04-20 02:21:21 +0430

    Update app module with all new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
-import { BooksModule } from './books/books.module';
-import { GenreModule } from './genre/genre.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './User/user.module';
+import BooksModule from './Books/books.module';
+import GenreModule from './Genre/genre.module';
+import UserEntity from './db/entity/user.entity';
+import BookEntity from './db/entity/book.entity';
+import GenreEntity from './db/entity/genre.entity';
 
 @Module({
-    imports: [HelloModule, BooksModule, GenreModule, UserModule],
+    imports: [UserModule,
+        BooksModule,
+        GenreModule,
+        TypeOrmModule.forFeature(
+            [UserEntity, BookEntity, GenreEntity],
+        ),
+
+        TypeOrmModule.forRoot(),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
