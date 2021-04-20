8a72acfc400362a8152382fa931ab1c402ecab83
Bardia Eghbali <bardia.eghbali@gmail.com>
2021-04-20 11:16:24 +0430

    Add documentation for all endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Header, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { BooksService } from './books.service';
 import CreateBookDto from './dto/create-book.dto';
 
@@ -6,11 +7,15 @@ import CreateBookDto from './dto/create-book.dto';
 export default class BooksController {
     constructor(private readonly booksService: BooksService) { }
 
+    @ApiOperation({ description: 'Creates a new book' })
+    @ApiCreatedResponse({ description: 'Book created successfully' })
     @Post()
     postBook(@Body() book: CreateBookDto) {
         return this.booksService.insert(book);
     }
 
+    @ApiOperation({ description: 'Retrieves list of all books' })
+    @ApiOkResponse({ description: 'Books retrieved successfully' })
     @Get()
     getAll() {
         return this.booksService.getAllBooks();
--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,14 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import GenreService from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('genres')
 export default class GenreController {
     constructor(private readonly genreService: GenreService) { }
+
+    @ApiOperation({ description: 'Creates a new genre' })
+    @ApiCreatedResponse({ description: 'Genre created successfully' })
     @Post()
     postGenre(@Body() genre: CreateGenreDto) {
         return this.genreService.insert(genre);
     }
+
+    @ApiOperation({ description: 'Retrieves list of all genres' })
+    @ApiOkResponse({ description: 'Genres retrieved successfully' })
     @Get()
     getAll() {
         return this.genreService.getAllGenre();
